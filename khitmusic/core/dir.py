e12a3ba4b1c98b035b66781cda951ca22f6ebbb5
Telegram & App source Code <khithlainhtet2021@gmail.com>
2026-03-05 20:28:46 +0630

    KHITMUSIC

---
+++ khitmusic/core/dir.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------
+# 🔸 khitmusic Project
+# 🔹 Developed & Maintained by: Stark (https://github.com/khithlainhtet)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by khithlainhtet# -----------------------------------------------
+import os
+from ..logging import LOGGER
+
+def dirr():
+    for file in os.listdir():
+        if file.endswith(".jpg"):
+            os.remove(file)
+        elif file.endswith(".jpeg"):
+            os.remove(file)
+        elif file.endswith(".png"):
+            os.remove(file)
+
+    if "downloads" not in os.listdir():
+        os.mkdir("downloads")
+    if "cache" not in os.listdir():
+        os.mkdir("cache")
+
+    LOGGER(__name__).info("Directories Updated.")
